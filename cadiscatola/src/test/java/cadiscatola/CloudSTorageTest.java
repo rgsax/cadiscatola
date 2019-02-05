d3067fbd2b93059f73807cfab870b8621f0308c0
Riccardo Giordano <riccardog.sax@gmail.com>
2019-02-05 17:38:23 +0100

    added JUnit4 on pom

---
+++ cadiscatola/src/test/java/cadiscatola/CloudSTorageTest.java
@@ -0,0 +1,14 @@
+package cadiscatola;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CloudSTorageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
